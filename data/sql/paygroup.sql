80c228f9868b1e57c3b718818e4f30c829b1c765
niravdiwan <ndiwan@illinois.edu>
2023-05-18 02:18:54 -0500

    Created the SQL Database using SQLite

---
+++ data/sql/paygroup.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS paygroup (
+    id TEXT,
+    remote_id INTEGER,
+    pay_group_name TEXT,
+    remote_was_deleted TEXT
+);
+INSERT INTO paygroup VALUES ('e3811390-eb58-4df1-b584-45cec0fb782a',94082417,'Intern','FALSE'),
+	('960fc07d-5498-4434-9ede-260f71953829',14527836,'Intern','TRUE'),
+	('04790b29-05b1-4940-a6c2-10a531eec967',38075219,'Part Time','FALSE'),
+	('741688e4-24af-40e9-a8c1-a17858b65182',16969302,'Part Time','FALSE'),
+	('888d5ee8-f2b8-4d26-af49-4d2db2dbd4cc',60159115,'Full Time','TRUE'),
+	('32fe1da2-17a9-45c1-a739-750b87ff3208',44695553,'Part Time','TRUE'),
+	('864829a5-e07d-4b8f-9071-114241cef12d',58005905,'Full Time','TRUE'),
+	('0d590557-b001-49b3-9bb7-9ced50d9f0f1',19846453,'Temp','FALSE'),
+	('565a25f6-c3a8-4170-898c-e2c849ed36e4',81443812,'Temp','TRUE'),
+	('87fdc45a-257f-40a3-96a9-d3ce4b678555',50179773,'Contractor','FALSE'),
+	('b5fad43e-0e69-45ee-ac2f-4781b560d69b',25315543,'Intern','FALSE'),
+	('d85019d4-19c2-4329-8dc2-fdb7e8ec68f9',14396625,'Temp','FALSE'),
+	('6c47f4a0-f48c-4736-ad24-d5559c374e01',72729901,'Intern','TRUE'),
+	('a2124a15-ee9f-4f95-842d-cc628b981aa7',13270787,'Full Time','TRUE'),
+	('3cc64f00-0a08-4ba0-86de-9336d27a5231',94726128,'Temp','FALSE'),
+	('99d0217a-a881-47a8-aaa5-60252b9b1f2a',18376598,'Full Time','TRUE'),
+	('263b3fa3-fd1a-4958-adef-14074b7bbb89',84449283,'Part Time','FALSE');
