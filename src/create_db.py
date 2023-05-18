9f654dc033adde7e8d28665c6fb4bb5cc618af9a
niravdiwan <ndiwan@illinois.edu>
2023-05-18 02:16:51 -0500

    Created the SQL database

---
+++ src/create_db.py
@@ -0,0 +1,35 @@
+import os
+from utils import dumpJsonFile, loadJsonFile, createSQLDB
+import sqlite3
+
+DB_FILEPATH = "../data/database/final_db.db"
+
+earning_db_path = "../data/sql/earning.sql"
+employee_db_path = "../data/sql/employee.sql"
+employeepayrollrun_db_path = "../data/sql/employeepayrollrun.sql"
+payrollrun_db_path = "../data/sql/payrollrun.sql"
+paygroup_db_path = "../data/sql/paygroup.sql"
+
+conn = sqlite3.connect(DB_FILEPATH)
+
+with open(earning_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+with open(earning_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+with open(employee_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+with open(employeepayrollrun_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+with open(payrollrun_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+with open(paygroup_db_path, 'r') as sql_file:
+    conn.executescript(sql_file.read())
+
+conn.close()
+
+
