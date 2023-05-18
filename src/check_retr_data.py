7a0fbe5baa38aaad77ae9112a3ca0a8cf6626206
niravdiwan <ndiwan@illinois.edu>
2023-05-18 16:34:59 -0500

    Including group_final in the db

--- src/check_retr_data.py
+++ src/check_retr_data.py
@@ -5,16 +5,18 @@ import pandas as pd
 import random
 
 # Table Name
-table_name = "employee"
+table_name = "group_final"
 
 # Loading SQL Database 
 DB_FILEPATH = "../data/database/final_db.db"
 conn = sqlite3.connect(DB_FILEPATH)
 
 # Load the example queries
-df = pd.read_csv("../data/example_queries/retr_set/" + table_name + ".csv", header="infer", delimiter="\t")
+df = pd.read_csv("../data/example_queries/complete_set/" + table_name + ".csv", header="infer", delimiter="|")
 
 final_examples = {}
+
+print(df.columns)
 
 # Check if the query is valid
 for index, row in df.iterrows():
@@ -45,5 +47,5 @@ test_data = pd.DataFrame(final_dataset[:5], columns = ['Question', 'SQL Query'])
 # Picking the rest as retrieval set
 retr_data = pd.DataFrame(final_dataset[5:], columns = ['Question', 'SQL Query'])
 
-test_data.to_csv("../data/example_queries/test_set/final_" + table_name + ".csv", sep="\t", index=False)
-retr_data.to_csv("../data/example_queries/retr_set/final_" + table_name + ".csv", sep="\t", index=False)
+test_data.to_csv("../data/example_queries/test_set/final_" + table_name + ".csv", sep="|", index=False)
+retr_data.to_csv("../data/example_queries/retr_set/final_" + table_name + ".csv", sep="|", index=False)
